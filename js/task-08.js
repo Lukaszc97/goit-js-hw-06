22da3e759375f56eea9308eebc701363232fbc92
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-02-20 17:35:30 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,17 @@
-const form = document.querySelector(".login-form");
-
+const form = document.querySelector(".login-form")
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
+  event.preventDefault()
   const email = form.elements.email.value;
-  const password = form.elements.password.value;
-
-  if (email === "" || password === "") {
-    alert("Please fill in all fields");
-  } else {
+  const password = form.elements.password.value
+  if (email === "" || password === "")
+  {alert("Upomnienij się, że wszystkie pola zostały wypełnione.");
+  }
+  else {
     const formData = {
       email: email,
-      password: password,
-    };
-    console.log(formData);
-    form.reset();
+      password: password
+    }
+    console.log(formData)
+    form.reset()
   }
-});
+})
