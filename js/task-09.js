5ec6236de13af6781c3da28be9f7c9d45d824c4a
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-02-20 16:06:23 +0100

    Create task-09.js

---
+++ js/task-09.js
@@ -0,0 +1,16 @@
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+function changeBackgroundColor() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  span.textContent = color;
+}
+
+button.addEventListener("click", changeBackgroundColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, "0")}`;
+}
