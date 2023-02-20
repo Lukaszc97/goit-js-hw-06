5e9abaab2aacd35f5addabfeae411aefea6d8e75
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-02-20 17:35:28 +0100

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
-const range = document.querySelector("#font-size-control");
-const text = document.querySelector("#text");
+const input = document.querySelector("#font-size-control");
+const output = document.querySelector("#text")
 
-range.addEventListener("input", (event) => {
-  text.style.fontSize = `${event.target.value}px`;
-});
+input.addEventListener("input", (event) => {
+  text.style.fontSize = `${event.target.value}px` 
+  
+})
