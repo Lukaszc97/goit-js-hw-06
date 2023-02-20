7dfdaabda813d6cea1b2fdbd6def2d8d202edb13
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-02-20 16:06:10 +0100

    Create task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,14 @@
+const counterValue = document.getElementById("value");
+let count = 0;
+
+document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.dataset.action === "decrement") {
+      count-+;
+    } else if (button.dataset.action === "increment") {
+      count++;
+    }
+
+    counterValue.textContent = count;
+  });
+});
