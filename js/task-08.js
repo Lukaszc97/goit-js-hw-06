e22c64d3da393260c862c39bb36b78dea9922524
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-02-22 18:47:32 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -4,7 +4,7 @@ form.addEventListener("submit", (event) => {
   const email = form.elements.email.value;
   const password = form.elements.password.value
   if (email === "" || password === "")
-  {alert("Upomnienij się, że wszystkie pola zostały wypełnione.");
+  {alert("Upewnij się, że wszystkie pola zostały wypełnione.");
   }
   else {
     const formData = {
